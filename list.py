c8fa5e7df2bd59b9c645ba298be971741a661628
IR-385\Santosh <santoshs@infinitiresearch.com>
2018-12-11 15:46:49 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,23 @@
+thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+print(len(thislist))
+
+thislist.append("mango")
+
+thislist.insert(2,"santoor")
+
+thislist.remove("cherry")
+
+thislist.pop()
+
+# thislist.clear()
+
+del thislist[0]
+
+print(thislist)
+
+# for x in thislist:
+# 	print(x)
+
+	
